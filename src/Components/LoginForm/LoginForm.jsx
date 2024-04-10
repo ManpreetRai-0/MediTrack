a34280eae0fdfb58ed4fd3fd778fafcc62945b27
ManpreetRai-0 <161531322+ManpreetRai-0@users.noreply.github.com>
2024-04-09 21:09:22 -0700

    code
    code

---
+++ src/Components/LoginForm/LoginForm.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import './LoginForm.css';
+
+import {FaUser} from "react-icons/fa";
+import {MdEmail} from "react-icons/md";
+import {RiLockPasswordFill} from "react-icons/ri";
+//import user_icon from '../Assets/profile.png';
+//import email_icon from '../Assets/email.png';
+//import lock_icon from '../Assets/lock.png';
+
+const LoginForm = () => {
+    return (
+        <div className="container">
+            <div className="header">
+                <div className="text">Login</div>
+                <div className="underline"></div>
+            </div>
+            <div className="inputs">
+                <div className="input">
+                    <FaUser />
+                    <input type="text" />
+                </div>
+                <div className="input">
+                    <MdEmail />
+                    <input type="email" />
+                </div>
+                <div className="input">
+                    <RiLockPasswordFill />
+                    <input type="password" />
+                </div>
+            </div>
+            <div className="forgot-password">Lost Password? <button>Click Here!</button></div>
+            <div className="submit-container">
+                <div className="submit"><button>Sign Up</button></div>
+                <div className="submit"><button>Login</button></div>
+            </div>
+        </div>
+    );
+};
+
+export default LoginForm;
